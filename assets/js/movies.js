59fbf5987da55f12113660b005f646fa02692c0d
Yali Miranda <yalimiranda@gmail.com>
2021-04-09 17:05:41 -0700

    initialized variables and added movie posters to assets

---
+++ assets/js/movies.js
@@ -0,0 +1,6 @@
+var movies =
+{
+    name: "Iron Man",
+    img: "assets/images/iron-man.jpg",
+    id: 0
+}
---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var moviePoster = document.getElementById("movie-poster");
+var winsDiv = document.getElementById("winsDiv");
+var wordDiv = document.getElementById("wordDiv");
+var guessesLeftDiv = document.getElementById("guessesLeftDiv");
+var guessListDiv = document.getElementById("guessListDiv");
+var numWins = 0;
+var word;
+var guessesLeft = 11;
+var guessList = [];
+var guessTracker = {};
+
+console.log(movies);
