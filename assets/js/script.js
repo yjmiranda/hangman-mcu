cb0a2296eeff9bc3222dc79c8169132dfde86541
yjmc25 <yalimiranda@gmail.com>
2021-04-10 00:31:32 -0700

    wrote function to populate the div of the word being guessed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,10 @@ var winsDiv = document.getElementById("winsDiv");
 var wordDiv = document.getElementById("wordDiv");
 var guessesLeftDiv = document.getElementById("guessesLeftDiv");
 var guessListDiv = document.getElementById("guessListDiv");
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numWins = 0; //current number of wins
 var word; //current word to guess
+var wordDivText = "";
 var movie; //current movie object
 var guessesLeft = 11; //number of guesses left
 var guessList = []; //a list of the letters guessed this game
@@ -15,4 +17,19 @@ const chooseRandomMovie = () => {
     word = movie.name;
 }
 
+const populateWordDiv = () => {
+    for(let i = 0; i < word.length; i++){
+        if(!alphabet.includes(word[i]) || guessTracker[word[i]]){
+            wordDivText += word[i];
+        } else if(!guessTracker[word[i]]){
+            wordDivText += "_ ";
+        } 
+    }
+    console.log(wordDivText);
+    wordDiv.innerHTML = wordDivText;
+}
+
 chooseRandomMovie();
+populateWordDiv();
+
+console.log(word);
