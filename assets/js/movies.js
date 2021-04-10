c916eb4f9003686b6306c1ca2f1442887be36612
yjmc25 <yalimiranda@gmail.com>
2021-04-09 23:50:52 -0700

    completed the movies object, added additional poster images

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -35,4 +35,89 @@ var movies =
         img: "assets/images/iron-man-3.jpg",
         id: 6
     },
+    {
+        name: "THOR: THE DARK WORLD",
+        img: "assets/images/thor-the-dark-world.jpg",
+        id: 7
+    },
+    {
+        name: "CAPTAIN AMERICA: THE WINTER SOLDIER",
+        img: "assets/images/captain-america-the-winter-soldier.jpg",
+        id: 8
+    },
+    {
+        name: "GUARDIANS OF THE GALAXY",
+        img: "assets/images/guardians-of-the-galaxy.jpg",
+        id: 9
+    },
+    {
+        name: "AVENGERS: AGE OF ULTRON",
+        img: "assets/images/avengers-age-of-ultron.jpg",
+        id: 10
+    },
+    {
+        name: "ANT-MAN",
+        img: "assets/images/ant-man.jpg",
+        id: 11
+    },
+    {
+        name: "CAPTAIN AMERICA: CIVIL WAR",
+        img: "assets/images/captain-america-civil-war.jpg",
+        id: 12
+    },
+    {
+        name: "DOCTOR STRANGE",
+        img: "assets/images/doctor-strange.jpg",
+        id: 13
+    },
+    {
+        name: "GUARDIANS OF THE GALAXY, VOL. 2",
+        img: "assets/images/guardians-of-the-galaxy-vol-2.jpeg",
+        id: 14
+    },
+    {
+        name: "SPIDER-MAN: HOMECOMING",
+        img: "assets/images/spider-man-homecoming.jpg",
+        id: 15
+    },
+    {
+        name: "THOR: RAGNAROK",
+        img: "assets/images/thor-ragnarok.png",
+        id: 16
+    },
+    {
+        name: "BLACK PANTHER",
+        img: "assets/images/black-panther.jpg",
+        id: 17
+    },
+    {
+        name: "AVENGERS: INFINITY WAR",
+        img: "assets/images/avengers-infinity-war.jpg",
+        id: 18
+    },
+    {
+        name: "ANT-MAN AND THE WASP",
+        img: "assets/images/ant-man-and-the-wasp.jpg",
+        id: 19
+    },
+    {
+        name: "CAPTAIN MARVEL",
+        img: "assets/images/captain-marvel.jpg",
+        id: 20
+    },
+    {
+        name: "AVENGERS: ENDGAME",
+        img: "assets/images/avengers-endgame.jpg",
+        id: 21
+    },
+    {
+        name: "SPIDER-MAN: FAR FROM HOME",
+        img: "assets/images/spider-man-far-from-home.jpg",
+        id: 22
+    },
+    {
+        name: "BLACK WIDOW",
+        img: "assets/images/black-widow.jpg",
+        id: 23
+    }
 ]
