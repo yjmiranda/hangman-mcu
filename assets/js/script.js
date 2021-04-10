ab3f91b7ac11f8af98fb4c23ba4471989f822d58
yjmc25 <yalimiranda@gmail.com>
2021-04-09 23:58:26 -0700

    wrote function to choose random movie

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,16 @@ var winsDiv = document.getElementById("winsDiv");
 var wordDiv = document.getElementById("wordDiv");
 var guessesLeftDiv = document.getElementById("guessesLeftDiv");
 var guessListDiv = document.getElementById("guessListDiv");
-var numWins = 0;
-var word;
-var guessesLeft = 11;
-var guessList = [];
-var guessTracker = {};
+var numWins = 0; //current number of wins
+var word; //current word to guess
+var movie; //current movie object
+var guessesLeft = 11; //number of guesses left
+var guessList = []; //a list of the letters guessed this game
+var guessTracker = {}; //trackes the letters that have been guessed this game
 
+const chooseRandomMovie = () => {
+    movie = movies[Math.floor(Math.random() * movies.length)];
+    word = movie.name;
+}
+
+chooseRandomMovie();
